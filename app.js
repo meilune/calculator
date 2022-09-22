21215ba8aef1f5a4f973f1f436a65ccb67080be8
Ginja <virginija.meilune@gmail.com>
2022-09-22 15:58:31 +1000

    Starting on the calculator functionality

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Testing")
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number) {
+    //If current display is zero, we want to replace it, if now add numbers
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === "0" ? number : displayValue + number;
+}
+
+//add Event listeners for numbers, operators and decimal buttons
+inputBtns.forEach((inputBtn) => {
+    if(inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if(inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if(inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    }
+})
